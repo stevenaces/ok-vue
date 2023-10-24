45648824a160aaa2fb5697ea0d2b348e326d40ca
xieshaojun <xieshaojun@aspirecn.com>
2023-10-24 20:45:15 +0800

    update: L07实现runner

---
+++ L07实现runner/reactivity/effect.ts
@@ -0,0 +1,58 @@
+
+class ReactiveEffect{
+  private _fn: any
+  constructor(fn){
+    this._fn = fn
+  }
+
+  run(){
+    activeEffect = this
+    return this._fn()
+  }
+}
+
+/* 收集/触发依赖（影响面）一个重要的点是如何设计数据结构来存储 */
+/*
+
+多个对象，根据每个对象的key来存储 依赖（影响面）
+{ // Map -> targetMap
+  "{obj}": { // Map -> depsMap
+    "key": [ effectFn / newReactiveEffect ] Set -> dep
+  }
+}
+
+*/
+
+const targetMap = new Map()
+
+export function track(target, key) {
+  let depsMap = targetMap.get(target)
+  if(!depsMap){
+    depsMap = new Map()
+    targetMap.set(target, depsMap)
+  }
+
+  let dep = depsMap.get(key)
+  if(!dep){
+    dep = new Set()
+    depsMap.set(key, dep)
+  }
+  dep.add(activeEffect)
+}
+
+export function trigger(target, key){
+  const depsMap = targetMap.get(target)
+  const dep = depsMap.get(key)
+  for (const effect of dep) {
+    effect.run()
+  }
+}
+
+let activeEffect;
+
+export function effect(fn) {
+  const _effect = new ReactiveEffect(fn)
+
+  _effect.run()
+  return _effect.run.bind(_effect) 
+}
